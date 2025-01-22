5fd0b0f1b0844370f7da45c40e3bd40996ca35fc
Lagicrus <Lagicrus@users.noreply.github.com>
2025-01-22 14:14:20 +0000

    Rewrite to merge into one test for video purposes Add labels Add additional tests

--- tests/src.spec.ts
+++ tests/src.spec.ts
@@ -1,60 +1,69 @@
 import { test, expect } from '@playwright/test';
 
-test("goto homepage", async ({ page }, testInfo) => {
+test("src full tests", async ({ page }, testInfo) => {
+  // Check 1 - Navigate to https://srcuk.com/
   await page.goto("/");
   // Get the baseURL from the config
   const baseURL = testInfo.config.projects[0].use.baseURL;
 
+  // Check 2 - read that the user is on the URL
   await expect(page).toHaveURL(baseURL);
-});
-
-// These are merged into one test as if you test the hover colour change without accepting cookies, the test will fail
-// Due to the cookie popup happening semi randomly during the test
-test("accept cookies & test header hover colour change", async ({ page }) => {
-  await page.goto("/");
 
   const acceptCookiesButton = page.getByRole("button", { name: "Accept cookies" });
   await acceptCookiesButton.click();
 
+  // Check 3 - ACCEPT COOKIES,
   await expect(acceptCookiesButton).not.toBeInViewport();
-
-  // PART 2
 
   const header = page.locator("header");
   const headerLinkText = await header.getByRole('link').all();
   headerLinkText.shift();
 
+  // Check 4 - The colour change on hover on all of the header labels
   for(const link of headerLinkText) {
     await link.hover();
     await expect(link).toHaveCSS("color", "rgb(239, 51, 64)");
   }
-})
 
-test("about us page", async ({ page }, testInfo) => {
-  await page.goto("/");
+  // Check 5 - Select About US
+
   await page.getByRole('banner').getByRole('link', { name: 'ABOUT US' }).click();
 
-  // Get the baseURL from the config
-  const baseURL = testInfo.config.projects[0].use.baseURL;
+  // Check 6 - Read that the user is on the URL https://srcuk.com/about-us/
   await expect(page).toHaveURL(`${baseURL}/about-us/`);
 
+  // Check 7 - Using playwright verify and validate the presence of aesthetic locators
   const aboutUsHeader = page.getByRole('heading', { name: 'ABOUT US' }).locator('span');
   await expect(aboutUsHeader).toBeVisible();
 
+  // Check 8 - On the About US page demonstrate playwright scroll to footer, scroll to header
   const footer = page.locator("footer");
   await footer.scrollIntoViewIfNeeded();
   await expect(footer).toBeInViewport();
 
   await aboutUsHeader.scrollIntoViewIfNeeded();
   await expect(aboutUsHeader).toBeInViewport();
-})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  // Check 9 - footer hover colours + ferrari
+  const footerLinkContainer = footer.locator('div[class*=\'grid-cols-3\']');
+  await expect(footerLinkContainer).toBeVisible();
+  const footerLinks = await footerLinkContainer.getByRole("link").all();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  for(const link of footerLinks) {
+    // We skip the facebook and linkedin links as their <a> link doesn't contain any classes, so they are
+    // tested next separately
+    if((await link.getAttribute('href')).match(/(facebook|linkedin)/i)) {
+      const linkIcon = link.locator("i");
+      await linkIcon.hover();
+      await expect(linkIcon).toHaveCSS("color", "rgba(255, 255, 255, 0.5)");
+      continue;
+    }
+    await link.hover();
+    await expect(link).toHaveCSS("color", "rgba(255, 255, 255, 0.5)");
+  }
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Check 10 - Ensure that the footer contains the text “SRC UK Limited Copyright © 2025.
+  // All rights Reserved.”
+
+  await expect(footer).toContainText("SRC UK Limited Copyright © 2025. All rights Reserved.");
 });
