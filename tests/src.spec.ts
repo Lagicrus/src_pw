61808150377076e988cae7cb0116bb9dddfc7615
Lagicrus <Lagicrus@users.noreply.github.com>
2025-01-22 14:30:27 +0000

    Add final tests

--- tests/src.spec.ts
+++ tests/src.spec.ts
@@ -64,6 +64,60 @@ test("src full tests", async ({ page }, testInfo) => {
 
   // Check 10 - Ensure that the footer contains the text “SRC UK Limited Copyright © 2025.
   // All rights Reserved.”
-
   await expect(footer).toContainText("SRC UK Limited Copyright © 2025. All rights Reserved.");
+
+  // Check 11 - Select Contact US
+  await page.getByRole('link', { name: 'Contact Us' }).click();
+
+  // Check 12 - Ensure the page navigates to URL https://srcuk.com/contact/
+  await expect(page).toHaveURL(`${baseURL}/contact/`);
+
+  // Check 13 - Ensure that Contact is on the page as well as every piece of text
+  const contactTitle = page.getByRole('heading', { name: 'CONTACT', exact: true }).locator('span');
+  await expect(contactTitle).toBeVisible();
+  const addressText = page.getByRole('heading', { name: 'ADDRESS' });
+  await expect(addressText).toBeVisible();
+  const contactFormText = page.getByRole('heading', { name: 'CONTACT FORM' });
+  await expect(contactFormText).toBeVisible();
+  const telephoneText = page.getByRole('heading', { name: 'TELEPHONE' });
+  await expect(telephoneText).toBeVisible();
+  await expect(footer).toContainText("SRC UK Limited Copyright © 2025. All rights Reserved.");
+
+  // Check 14 - Ensure playwright can add text to the Contact Form
+  const yourNameInput = page.locator('input[name=\'your-name\']');
+  await yourNameInput.fill('John Doe');
+  await expect(yourNameInput).toHaveValue('John Doe');
+
+  // Check 15 - Ensure when the fields are empty and the playwright selects Submit every
+  // field presents with a red border and red writing with text Please fill out this
+  // field.
+  await yourNameInput.fill('');
+  const submitButton = page.getByRole('button', { name: 'Submit' });
+  await submitButton.click();
+
+  // Setup the locators for the error messages & inputs
+  const yourEmailInput = page.locator('input[name=\'your-email\']');
+  const yourSubjectInput = page.locator('input[name=\'your-subject\']');
+  const yourMessageInput = page.locator('textarea[name=\'your-message\']');
+
+  const yourNameLabel = page.locator('span[data-name=\'your-name\']');
+  const yourEmailLabel = page.locator('span[data-name=\'your-email\']');
+  const yourSubjectLabel = page.locator('span[data-name=\'your-subject\']');
+  const yourMessageLabel = page.locator('span[data-name=\'your-message\']');
+
+  // Test aria-invalid, border-color, and text content
+  await expect(yourNameInput).toHaveAttribute('aria-invalid', 'true');
+  await expect(yourEmailInput).toHaveAttribute('aria-invalid', 'true');
+  await expect(yourSubjectInput).toHaveAttribute('aria-invalid', 'true');
+  await expect(yourMessageInput).toHaveAttribute('aria-invalid', 'true');
+
+  await expect(yourNameInput).toHaveCSS('border-color', 'rgb(220, 38, 38)');
+  await expect(yourEmailInput).toHaveCSS('border-color', 'rgb(220, 38, 38)');
+  await expect(yourSubjectInput).toHaveCSS('border-color', 'rgb(220, 38, 38)');
+  await expect(yourMessageInput).toHaveCSS('border-color', 'rgb(220, 38, 38)');
+
+  await expect(yourNameLabel).toContainText('Please fill out this field.');
+  await expect(yourEmailLabel).toContainText('Please fill out this field.');
+  await expect(yourSubjectLabel).toContainText('Please fill out this field.');
+  await expect(yourMessageLabel).toContainText('Please fill out this field.');
 });
